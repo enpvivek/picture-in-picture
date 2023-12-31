5d43fcb556dcf8a3edbf017657d77cad089f4c56
enpvivek <enpvivek@gmail.com>
2023-12-31 12:39:04 +0530

    complete frontend

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        white: "#ffffff",
+        lightGray: "#efefef",
+        black: "#000000",
+        primary: "#2D4356",
+        secondary: "#435B66",
+        danger: "#A76F6F",
+        bermuda: "#78dcca",
+      },
+    },
+  },
+  plugins: [],
+};
